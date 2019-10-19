f91f74cc12a7ae4fa3a4776864d6e7840e2032da
daissh04 <nortondiss04@ezweb.ne.jp>
2019-10-19 14:30:31 +0900

    Auto-Update

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,12 +1,29 @@
 $(document).on("turbolinks:load",function(){
   function buildMessage(message){ 
-    var html =`<div class="chat-main__message__box">
+  if (message.content && message.image.url){
+    var html = `<div class="chat-main__message__box" data-message-id='${message.id}'>
     <p class="chat-main__message__box__user-name">${message.name}</p>
     <p class="chat-main__message__box__date">${message.created_at}</p>
     <p class="chat-main__message__box__text">${message.content}</p>
     <div id="lower-message__image"><img src=${message.image}></div>
     </div>`
-    return html;
+  }
+  else if(message.content){
+    var html = `<div class="chat-main__message__box" data-message-id='${message.id}'>
+    <p class="chat-main__message__box__user-name">${message.name}</p>
+    <p class="chat-main__message__box__date">${message.created_at}</p>
+    <p class="chat-main__message__box__text">${message.content}</p>
+    </div>`
+  }
+  
+  else if(message.image.url){
+    var html = `<div class="chat-main__message__box" data-message-id='${message.id}'>
+    <p class="chat-main__message__box__user-name">${message.name}</p>
+    <p class="chat-main__message__box__date">${message.created_at}</p>
+    <div id="lower-message__image"><img src=${message.image}></div>
+    </div>`
+  };
+  return html;
   }
 
   $('#new_message').on('submit',function(e){
@@ -37,73 +54,61 @@ $(document).on("turbolinks:load",function(){
 });
 
 $(document).on("turbolinks:load",function(){
-  var search_list = $('#user-search-result');
-  function appendMember(members){
-  var html = `<div id="user-search-result">
-    <div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">${members.name}</p>
-      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${members.id} data-user-name=${members.name}>追加</a>
-    </div>
-  </div>`
-  search_list.append(html);
-  }
 
-  function appendErrMsgToHTML(error){
-    var html = `<div id="user-search-result">
-      <div class="chat-group-user clearfix">
-        <p class="chat-group-user__name">${error}</p>
+
+  function buildMessageHTML(message){
+  if (message.content && message.image.url){
+    var insertHTML = `<div class="chat-main__message__box" data-message-id='${message.id}'>
+    <p class="chat-main__message__box__user-name">${message.name}</p>
+    <p class="chat-main__message__box__date">${message.created_at}</p>
+    <p class="chat-main__message__box__text">${message.content}</p>
+    <div id="lower-message__image"><img src=${message.image}></div>
     </div>`
-    search_list.append(error);
-    }
-
-  function addMember(user,id){
-    var memberHtml = `<div id="user-search-result">
-    <div class="chat-group-user clearfix">
-      <input name="group[user_ids][]" type="hidden" value='${id}'>
-      <p class="chat-group-user__name">${user}</p>
-      <a class="user-search-add chat-group-user__btn chat-group-user__btn--remove" js-remove-btn'>削除</a>
-    </div>
-  </div>`
-  $('.member__list').append(memberHtml)
   }
+  else if(message.content){
+    var insertHTML = `<div class="chat-main__message__box" data-message-id='${message.id}'>
+    <p class="chat-main__message__box__user-name">${message.name}</p>
+    <p class="chat-main__message__box__date">${message.created_at}</p>
+    <p class="chat-main__message__box__text">${message.content}</p>
+    </div>`
+  }
+  
+  else if(message.image.url){
+    var insertHTML = `<div class="chat-main__message__box" data-message-id='${message.id}'>
+    <p class="chat-main__message__box__user-name">${message.name}</p>
+    <p class="chat-main__message__box__date">${message.created_at}</p>
+    <div id="lower-message__image"><img src=${message.image}></div>
+    </div>`
+  }
+  $('.chat-main__message').append(insertHTML)
+  };
 
-$(function(){
-  $('#user-search-field').on("keyup", function(){
-    var input = $('#user-search-field').val();
+  
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var last_message_id = $('.chat-main__message__box:last').data('message-id')
+    var group_id = $('.chat-main__main-header__left-box').data('group-id')
+    var href = `/groups/${group_id}/api/messages`
+    console.log(last_message_id)
     $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input},
-      dataType: 'json'
+      url: href,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
     })
-
-    .done(function(members){
-      $('#user-search-result').empty();
-      if (members.length !==0){
-        members.forEach(function(members){
-          appendMember(members);
-        });
-      }
-      else{
-        appendErrMsgToHTML("一致するメンバーはいません");
+    .done(function(messages) {
+      if(messages.length !==0){
+        messages.forEach(function(messages){
+          buildMessageHTML(messages)
+          console.log(messages)
+          $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0].scrollHeight })
+        })
       }
     })
-  })
-})
-
-$('#user-search-result').on('click', '.chat-group-user__btn--add', 
-function(){
-  var user = $(this).data('user-name');
-  var id = $(this).data('user-id');
-  addMember(user, id)
-  $(this).parent().remove()
+    .fail(function() {
+      console.log('error');
+    })
+    }
+  }
+  setInterval(reloadMessages, 5000);
 });
-
-$('.member__list').on('click', '.chat-group-user__btn--remove', 
-function(){
-  $(this).parent().remove()
-});
-
-});
-
-
